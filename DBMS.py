5bcd6b0974744372d6ffb38cd643b8c74a96bc05
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2024-03-07 01:57:42 +0530

    DBMS functionable

--- DBMS.py
+++ DBMS.py
@@ -4,103 +4,125 @@ from paths import DATABASE_PATH, EXPENSE_DATABASE_DIR
 from datetime import datetime
 import calendar
 
-def get_current_date():
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    return current_date
+class DBMS:
+    def __init__(self, database_path, expense_database_directory):
+        self.__DATABASE_PATH = database_path
+        self.__EXPENSE_DATABASE_DIRECTORY = expense_database_directory
 
-def saveCategory(name):
-    conn = connect(DATABASE_PATH)
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, category TEXT)")
-    conn.commit()
-    cur.execute("SELECT * FROM categories WHERE category=?", (name,))
-    existing_category = cur.fetchone()
-
-    if not existing_category:
-        cur.execute("INSERT INTO categories (category) VALUES (?)", (name,))
-        conn.commit()
-    conn.close()
-
-def saveIncome(year, month, income):
-    months_array = calendar.month_name[1:]
-    conn = connect(DATABASE_PATH)
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS income (id INTEGER PRIMARY KEY, year INTEGER, month TEXT, income REAL)")
-    conn.commit()
-    cur.execute(f"INSERT INTO income (year, month, income) VALUES ({int(year)}, '{str(months_array[int(month) - 1])}', {float(income)})")
-    conn.commit()
-    conn.close()
-
-def getIncome(year, month):
-    months_array = calendar.month_name[1:]
-    conn = connect(DATABASE_PATH)
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM income WHERE year={int(year)} AND month='{str(months_array[month] - 1)}'")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-
-
-def saveExpense(amount, description, category, date_of_expense, type):
-    months_array = calendar.month_name[1:]
-    de = date_of_expense.split("-")
-    conn = connect(os.path.join(EXPENSE_DATABASE_DIR, f'{de[0]}.db'))
-    cur = conn.cursor()
-    cur.execute(f"CREATE TABLE IF NOT EXISTS {months_array[int(de[1]) - 1]} (id INTEGER PRIMARY KEY, amount REAL, date_of_expense DATE, category TEXT, description TEXT, date_of_registration DATE, type TEXT)")
-    conn.commit()
-    current_date = get_current_date()
-    query = f"INSERT INTO {months_array[int(de[1]) - 1]} (amount, date_of_expense, category, description, date_of_registration, type) VALUES ({float(amount)}, '{date_of_expense}', '{str(category)}', '{str(description)}', '{current_date}', '{str(type)}')"
+    def __get_current_date(self):
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        return current_date
     
-    cur.execute(query)
-    conn.commit()
-    conn.close()
+    def __get_months_list(self):
+        return calendar.month_name[1:]
 
-def getData(year=False, month=False, DB=False):
-    if not DB:
-        if not year:
-            print("Please provide a year or database name.")
-            return None
-        conn = connect(os.path.join(EXPENSE_DATABASE_DIR, f"{year}.db"))
-    else:
-        conn = connect(os.path.join(EXPENSE_DATABASE_DIR, DB))
+    def __is_specific_income_available(self, year, month):
+        if os.path.isfile(self.__DATABASE_PATH):
+            conn = connect(self.__DATABASE_PATH)
+            cur = conn.cursor()
+            cur.execute(f"SELECT COUNT(*) FROM income WHERE year={int(year)} AND month='{str(self.__get_months_list()[int(month) - 1])}'")
+            count = cur.fetchone()[0]
+            conn.close()
+            return count > 0
 
-    cur = conn.cursor()
-    all_data = {}
+    def __is_expense_available(self, file_name, month, id):
+        if os.path.isfile(os.path.join(self.__EXPENSE_DATABASE_DIRECTORY, file_name)):
+            conn = connect(os.path.join(self.__EXPENSE_DATABASE_DIRECTORY, file_name))
+            cur = conn.cursor()
+            cur.execute(f"SELECT COUNT(*) FROM {self.__get_months_list()[int(month) - 1]} WHERE id='{str(id)}'")
+            count = cur.fetchone()[0]
+            conn.close()
+            return count > 0
 
-    # Fetch all tables and their data
-    if not month and not year:
-        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cur.fetchall()
+    def addIncome(self, amount, date = False):
+        date = date.split("-") if not date is False else self.__get_current_date().split("-")
+        conn = connect(self.__DATABASE_PATH)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS income (id INTEGER PRIMARY KEY, year INTEGER, month TEXT, income REAL, expense REAL, date DATE)")
+        conn.commit()
+        is_income_added = self.__is_specific_income_available(int(date[0]), str(self.__get_months_list()[int(date[1]) - 1]))
+        if is_income_added:
+            conn.close()
+            return f"Income already added for {str(self.__get_months_list()[int(date[1]) - 1])}"
+        cur.execute(f"INSERT INTO income (year, month, income, expense, date) VALUES ({int(date[0])}, '{str(self.__get_months_list()[int(date[1]) - 1])}', {float(amount)}, {float(0.00)}, '{'-'.join(date)}')")
+        conn.commit()
+        conn.close()
 
-        for table in tables:
-            table_name = table[0]
-            cur.execute(f"SELECT * FROM {table_name};")
-            rows = cur.fetchall()
-            all_data[table_name] = rows
-    else:
-        # Fetch data based on the provided conditions
-        if month:
-            cur.execute(f"SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cur.fetchall()
+    def addExpense(self, amount, category, description, type = "negative", date = False, edit = False, id = None):
+        date = date.split("-") if not date is False else self.__get_current_date().split("-")
+        is_income_available = self.__is_specific_income_available(int(date[0]), int(date[1]))
+        if is_income_available:
+            conn = connect(os.path.join(self.__EXPENSE_DATABASE_DIRECTORY, f"{date[0]}.db"))
+            cur = conn.cursor()
+            cur.execute(f"CREATE TABLE IF NOT EXISTS {self.__get_months_list()[int(date[1]) - 1]} (id INTEGER PRIMARY KEY, amount REAL, date_of_expense DATE, category TEXT, description TEXT, date_of_registration DATE, type TEXT)")
+            conn.commit()
+            if edit:
+                is_income_available = self.__is_expense_available(f"{date[0]}.db", date[1], id)
+                if not is_income_available:
+                    conn.close()
+                    return "Can't edit an non existing expense"
+            else:
+                    cur.execute(f"INSERT into {self.__get_months_list()[int(date[1]) - 1]} (amount, date_of_expense, category, description, date_of_registration, type) VALUES ({float(amount)}, '{"-".join(date)}', '{str(category)}', '{str(description)}', '{str(self.__get_current_date())}', '{str(type)}')")
+                    conn.commit()
+                    conn.close()
+                    conn2 = connect(self.__DATABASE_PATH)
+                    cur2 = conn2.cursor()
+                    old_value = cur2.execute(f"SELECT {"expense" if type == "negative" else "income"} FROM income WHERE year={int(date[0])} AND month='{str(self.__get_months_list()[int(date[1]) - 1])}'")
+                    old_value = old_value.fetchall()[0][0]
+                    conn2.commit()
+                    new_value = float(old_value) + float(amount)
+                    cur2.execute(f"UPDATE income SET {"expense" if type == "negative" else "income"}={float(new_value)} WHERE year={int(date[0])} AND month='{self.__get_months_list()[int(date[1]) - 1]}'")
+                    conn2.commit()
+                    conn2.close()
+        else:
+            return f"Income not available for month {self.__get_months_list()[int(date[1]) - 1]}"
+            
+    def trial(self, date = False):
+        date = date.split("-") if not date is False else self.__get_current_date().split("-")
+        conn2 = connect(self.__DATABASE_PATH)
+        cur2 = conn2.cursor()
+        hel = cur2.execute(f"SELECT expense FROM income WHERE year={int(date[0])} AND month='{str(self.__get_months_list()[int(date[1]) - 1])}'")
+        hel = hel.fetchall()
+        conn2.commit()
+        conn2.close()
+        return hel
 
-            for table in tables:
-                table_name = table[0]
-                if table_name.lower() == month.lower():
-                    cur.execute(f"SELECT * FROM {table_name};")
-                    rows = cur.fetchall()
-                    all_data[table_name] = rows
 
-        elif year:
-            cur.execute(f"SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cur.fetchall()
+dbm = DBMS(DATABASE_PATH, EXPENSE_DATABASE_DIR)
+# income = dbm.addIncome(200000, "2024-01-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2024-02-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2024-02-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2023-01-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2023-01-23")
+# print(income if not income is None else "Income successfully added")
 
-            for table in tables:
-                table_name = table[0]
-                if table_name.startswith(year):
-                    cur.execute(f"SELECT * FROM {table_name};")
-                    rows = cur.fetchall()
-                    all_data[table_name] = rows
+# exp = dbm.addExpense(2000, "gaming", "Purchased GTA V", "negative", id=2, date="2024-01-20")
+# exp = dbm.addExpense(2000, "gaming", "Purchased GTA V", "negative", id=2, date="2024-02-20")
+# exp = dbm.addExpense(2000, "gaming", "Purchased GTA V", "negative", id=2, date="2024-02-20")
+# exp = dbm.addExpense(2000, "gaming", "Purchased GTA V", "negative", id=2, date="2023-01-20")
+# exp = dbm.addExpense(2000, "gaming", "Purchased GTA V", "negative", id=2, date="2023-01-23")
+# exp = dbm.addExpense(10000, "gaming", "Purchased GTA V", "negative", id=2, date="2024-01-23")
+# print(exp)
+# exp = dbm.addExpense(1000, "rent", "Company rent", "positive", date="2024-01-23")
+# print(exp)
 
-    conn.close()
 
-    return all_data
+# print(dbm.trial("2024-02-20")[0][0])
+
+
+# income = dbm.addIncome(200000, "2024-01-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2024-02-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2024-02-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2023-01-20")
+# print(income if not income is None else "Income successfully added")
+# income = dbm.addIncome(200000, "2023-01-23")
+# print(income if not income is None else "Income successfully added")
+
+# print(exp)
--- main.py
+++ main.py
@@ -1,56 +1,15 @@
-import sqlite3
-import datetime
+import os
+from DBMS import DBMS
+from paths import DATABASE_PATH, EXPENSE_DATABASE_DIR, DATABASE_DIR
 
-conn = sqlite3.connect("expenses.db")
-cur = conn.cursor()
+def createDir(name):
+    os.makedirs(name)
+    print(f"Directory '{name}' created successfully.")
 
-while True:
-    print("Select any option")
-    print("1. Enter new expense")
-    print("2. View expenses")
+if not os.path.isdir(DATABASE_DIR):
+    createDir(DATABASE_DIR)
 
-    choice = int(input(": "))
+if not os.path.isdir(EXPENSE_DATABASE_DIR):
+    createDir(EXPENSE_DATABASE_DIR)
 
-    if choice == 1:
-        date = input("Enter the date of the expennse(YYYY-MM-DD): ")
-        description = input("Enter the description of expense")
-
-        cur.execute("SELECT DISTINCT category FROM expenses")
-
-        categories = cur.fetchall()
-
-        print("Select a category from number")
-
-        for idx, category in enumerate(categories):
-            print(f"{idx + 1}. {category[0]}")
-        
-        print(f"{len(categories) + 1}. Create a new category")
-
-        category_choice = int(input(": "))
-
-        if category_choice == len(categories) + 1:
-            category = input("Enter the new category name: ")
-        else: category = categories[category_choice - 1][0]
-
-        price = input("Enter the price of the expenses: ")
-
-        cur.execute("INSERT INTO expenses (date, description, category, price) VALUES (?, ?, ?, ?)", (date, description, category, price))
-
-        conn.commit()
-
-    elif choice == 2:
-        print("Select an option:")
-        print("1. View all expenses")
-        # print("2. View monthly expenses by category")
-        view_choice = int(input(": "))
-        # if view_choice == 1:
-        cur.execute("SELECT * FROM expenses")
-        expenses = cur.fetchall()
-        for expense in expenses:
-            print(expense)
-        # elif view_choice == 2:
-        #     pass
-        # else:
-        #     exit()
-    else:
-        exit()
+database = DBMS(DATABASE_PATH, EXPENSE_DATABASE_DIR)
